8ff495a104687cc1eb977e3c6f025d703eef958c
yotsufe <yotsufe@gmail.com>
2021-08-10 02:23:32 +0900

    マテリアルデザイン

--- app/src/main/java/com/yotsufe/androidjetpackcomposeresearch/MainActivity.kt
+++ app/src/main/java/com/yotsufe/androidjetpackcomposeresearch/MainActivity.kt
@@ -3,9 +3,18 @@ package com.yotsufe.androidjetpackcomposeresearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +27,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    val typography = MaterialTheme.typography
+    Column(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.header),
+            contentDescription = null,
+            modifier = Modifier
+                .height(180.dp)
+                .fillMaxWidth()
+                .clip(shape = RoundedCornerShape(16.dp)),
+            contentScale = ContentScale.Crop
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        Text(text = "Hello $name!",
+            style = typography.h6)
+        Text(text = "description",
+            style = typography.body2)
+    }
 }
 
 @Preview(showBackground = true)
